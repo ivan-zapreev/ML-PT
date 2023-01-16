3d4dece15d668cd33fe443c6e1752d75ff4b780f
Dr. Ivan S. Zapreev <ivan.zapreev@gmail.com>
2023-01-16 21:32:48 +0300

    Clean-up

--- src/utils/file_utils.py
+++ src/utils/file_utils.py
@@ -7,22 +7,6 @@ import _pickle as cPickle
 
 from src.utils.logger import logger
 
-# Pickle a file and then compress it into a file with extension 
-def compressed_pickle(folder, file_name, data):
-    file_path = os.path.join(folder, f'{file_name}.pbz2')
-    logger.info(f'Dumping compressed pickle file into: {file_path}')
-    with bz2.BZ2File(file_path, 'w') as f: 
-        cPickle.dump(data, f)
-    logger.info(f'File dumping is done!')
-
-def decompress_pickle(folder, file_name):
-    file_path = os.path.join(folder, f'{file_name}.pbz2')
-    logger.info(f'Loading compressed pickle file from: {file_path}')
-    data = bz2.BZ2File(file_path, 'rb')
-    data = cPickle.load(data)
-    logger.info(f'File loading and data extraction are done!')
-    return data
-
 def store_numpy_zc(file_path, **kwds):
     logger.info(f'Dumping compressed numpy z file into: {file_path}.npz')
     np.savez_compressed(file_path, **kwds)
